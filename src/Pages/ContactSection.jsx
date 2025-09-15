5139e45c75b0e8d6cf751b3c822ab5c7defb1403
Olofin Abiodun <olofinabiodun42@gmail.com>
2025-09-15 08:13:54 +0100

    update

--- src/Pages/ContactSection.jsx
+++ src/Pages/ContactSection.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Card, CardContent } from "../components/ui/card"
+import { Card, CardContent } from "../components/ui/Card"
 import { Button } from "../components/ui/button"
 import { Mail, Phone, MapPin } from "lucide-react"
 import NavBar from "../components/NavBar"
